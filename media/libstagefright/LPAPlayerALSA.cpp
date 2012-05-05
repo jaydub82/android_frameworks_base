gp-b2g/frameworks_base

libstagefright: honor write done event during pause
Queue up completed buffers for decoding even if playback
is in paused state as there is concurrency between write
done and pause

Change-Id: I74ac1ccda4edaf48a7a5de825ce4742ff5fa6b61
CRs-Fixed: 340469

media/libstagefright/LPAPlayerALSA.cpp
@@ -967,7 +967,7 @@ void LPAPlayer::eventThreadEntry() {
         if (killEventThread) {
             break;
         }
-        if (isPaused || memBuffersResponseQueue.empty())
+        if (memBuffersResponseQueue.empty())
             continue;
 
         //exit on abrupt event

jaydub82/android_frameworks_base

(cherry picked from commit c9116b67545c5d973c255fba55c031271d3c38a4)
Change-Id: Ifdc2cec4d92773ac279c02df7067bd95c32ca4a4

media/libstagefright/LPAPlayerALSA.cpp
@@ -970,7 +970,7 @@ void LPAPlayer::eventThreadEntry() {
         if (killEventThread) {
             break;
         }
-        if (isPaused || pmemBuffersResponseQueue.empty())
+        if (pmemBuffersResponseQueue.empty())
             continue;
 
         //exit on abrupt event